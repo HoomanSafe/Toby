82e28640e0ee74a86d28eb89edb5035e4b320403
Arc <atandarokib@gmail.com>
2023-07-17 18:44:45 +0100

    up

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -8,9 +8,9 @@ const Features = () => {
           Understand Your Power
         </h1>
         <p className="font-spicyRice mt-20 text-center text-white text-3xl lg:text-5xl max-w-3xl font-normal">
-          100% OF THE TOTAL SUPPLY HAS BEEN LOCKED ON UNISWAP AND LIQUIDITY
-          TOKENS HAS BEEN BURNED. $CF WAS A FAIR LAUNCH BLOCKCHAIN SMART
-          CONTRACT ECOSYSTEM.
+          SharkDogs travel in packs! A simple glance can be taken as an act of
+          aggression. Where weaknesses lie, strengths override and when one
+          attacks they all attack.
         </p>
         <div className="justify-start font-spicyRice flex-col md:flex-row items-start gap-[30px] inline-flex">
           <div className="px-8 py-4 rounded-[100px] shadow border border-amber-400 justify-center items-center gap-2.5 flex">
