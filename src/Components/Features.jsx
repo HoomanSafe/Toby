437fdf01dbbb51af2f718a85ab2965cbea2c9bfd
Arc <atandarokib@gmail.com>
2023-07-17 18:42:45 +0100

    up

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -5,7 +5,7 @@ const Features = () => {
     <section className="py-20 w-full">
       <div className="container gap-8 relative flex flex-col justify-center items-center">
         <h1 className="text-6xl top-0 absolute md:text-8xl text-center font-spicyRice tracking-[5.60px] yesOut">
-          PEPE AND TOAD
+          Understand Your Power
         </h1>
         <p className="font-spicyRice mt-20 text-center text-white text-3xl lg:text-5xl max-w-3xl font-normal">
           100% OF THE TOTAL SUPPLY HAS BEEN LOCKED ON UNISWAP AND LIQUIDITY
