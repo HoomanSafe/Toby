6baf60e5b40fc552fee4d24967ac0c9882f227be
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-22 18:07:39 +0800

    Update Features.jsx

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -5,21 +5,20 @@ const Features = () => {
     <section className="py-20 w-full">
       <div className="container gap-8 relative flex flex-col justify-center items-center">
         <h1 className="text-6xl top-0 lg:absolute md:text-8xl text-center font-spicyRice tracking-[5.60px] yesOut">
-          Understand Your Power
+          Adventurer : Toby ($TOBY) | 🪙BNB
         </h1>
         <p className="font-spicyRice mt-20 text-center text-white text-3xl lg:text-5xl max-w-3xl font-normal">
-          SharkDogs travel in packs! A simple glance can be taken as an act of
-          aggression. Where weaknesses lie, strengths override and when one
-          attacks they all attack.
+          Each token has its own use and can be used for trading, gaming and advertising within the Toby community.
+          
         </p>
         <div className="justify-start font-spicyRice flex-col md:flex-row items-start gap-[30px] inline-flex">
           <div className="px-8 py-4 rounded-[100px] shadow border border-amber-400 justify-center items-center gap-2.5 flex">
             <div className="text-white text-xl font-normal leading-7 tracking-tight">
-              BUY SHARK DOG
+              BUY TOBY
             </div>
           </div>
           <a
-            href="https://t.me/SharkDogUnleashed"
+            href="https://t.me/Tobybnb"
             className="px-8 py-4 rounded-[100px] shadow border border-amber-400 justify-center items-center gap-2.5 flex"
           >
             <div className="text-white text-xl font-normal leading-7 tracking-tight">
